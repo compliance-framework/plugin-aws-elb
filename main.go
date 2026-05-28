063dfc1540bd61e4384c5ab4dcb533595324f95f
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2026-05-28 08:41:17 -0300

    fix: removing dashes
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ import (
 var defaultPolicyBehaviors = map[string][]string{
 	"plugin-aws-elb-loadbalancer-policies":  {resourceTypeLoadBalancer},
 	"plugin-aws-elb-listener-policies":      {resourceTypeListener},
-	"plugin-aws-elb-target-group-policies":  {resourceTypeTargetGroup},
-	"plugin-aws-elb-target-health-policies": {resourceTypeTargetHealth},
+	"plugin-aws-elb-targetgroup-policies":  {resourceTypeTargetGroup},
+	"plugin-aws-elb-targethealth-policies": {resourceTypeTargetHealth},
 }
 
 func requestWithDefaultPolicyBehavior(req *proto.EvalRequest) *proto.EvalRequest {
